2a2ca1fd9130b42c061fb5ca1607f85f74cdba02
Dean Williams <dean.williams@americanstationery.com>
2020-02-05 15:21:52 -0500

    fix for admin routing 6788

--- src/app/code/community/Uni/Banner/controllers/Adminhtml/Banner/BannerController.php
+++ src/app/code/community/Uni/Banner/controllers/Adminhtml/Banner/BannerController.php
@@ -7,7 +7,7 @@
  * @copyright  Copyright (c) 2010-2011 Unicode Systems. (http://www.unicodesystems.in)
  * @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
-class Uni_Banner_Adminhtml_BannerController extends Mage_Adminhtml_Controller_Action
+class Uni_Banner_Adminhtml_Banner_BannerController extends Mage_Adminhtml_Controller_Action
 {
 
     protected function _initAction()
--- src/app/code/community/Uni/Banner/controllers/Adminhtml/Banner/BannergroupController.php
+++ src/app/code/community/Uni/Banner/controllers/Adminhtml/Banner/BannergroupController.php
@@ -6,7 +6,7 @@
  * @copyright  Copyright (c) 2010-2011 Unicode Systems. (http://www.unicodesystems.in)
  * @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
-class Uni_Banner_Adminhtml_BannergroupController extends Mage_Adminhtml_Controller_Action {
+class Uni_Banner_Adminhtml_Banner_BannergroupController extends Mage_Adminhtml_Controller_Action {
 
     protected function _initAction() {
         $this->loadLayout()
